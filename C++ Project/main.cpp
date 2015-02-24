60ed5dfb67a5d0a5c2c702521565423bfa271801
David Zhou <david.zhou@jumptap.com>
2015-02-23 22:14:26 -0500

    Initial Commit

---
+++ C++ Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++ Project
+//
+//  Created by David.Zhou on 2/23/15.
+//  Copyright (c) 2015 David.Zhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
